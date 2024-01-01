8e6733143ba754c30a04572d0cd1982ff17b36b4
“thaotran“ <phtranthao@gmail.com>
2024-01-01 21:06:05 +0100

    Display digits

--- script.js
+++ script.js
@@ -32,3 +32,11 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+let tmp = 0;
+
+//Display numbers
+const display = document.querySelector('.display');
+let digitButtons = document.querySelectorAll('.digit').forEach(btn => btn.addEventListener('click', function(){
+    display.innerHTML += btn.innerHTML;
+}));
